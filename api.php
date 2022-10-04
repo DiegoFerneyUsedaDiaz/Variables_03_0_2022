389f21221f6f907d69e81557ab9e35bd63bbdfd3
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2022-10-03 21:24:33 -0500

    Actualizacion Readme

--- api.php
+++ api.php
@@ -1,12 +1,12 @@
 <?php
 
-header(Access-Control-Allow-Origin: *);
+header('Access-Control-Allow-Origin: *');
 
 $nombreCompleto = "Diego Ferney Useda Diaz";
 $edad = 36;
 $altura = 1.75;
 $soyAlumno = true;
-$pasaTiempo = ["Dibujar" ,"Hacer Ejercicio" , "Video Juegos" , "Investigar"]
+$pasaTiempo = ["Dibujar" ,"Hacer Ejercicio" , "Video Juegos" , "Investigar"];
 $direccion = new stdClass();
 $direccion ->principal = "Calle 105";
 $direccion ->secundaria = "Finca el Porvenir";
